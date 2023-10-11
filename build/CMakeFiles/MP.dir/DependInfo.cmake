db4dc329b108797b4505cea0d8e44276c660f02d
charlesmowbray <charlesmowbray@mines.edu>
2023-10-11 10:06:38 -0600

    trying to add dorock, not finished

---
+++ build/CMakeFiles/MP.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/charlesmowbray/Documents/CSCI441/midtermProject/groupProject/CSCI441MP/ArcballCam.cpp" "CMakeFiles/MP.dir/ArcballCam.cpp.o" "gcc" "CMakeFiles/MP.dir/ArcballCam.cpp.o.d"
+  "/home/charlesmowbray/Documents/CSCI441/midtermProject/groupProject/CSCI441MP/Bardo.cpp" "CMakeFiles/MP.dir/Bardo.cpp.o" "gcc" "CMakeFiles/MP.dir/Bardo.cpp.o.d"
+  "/home/charlesmowbray/Documents/CSCI441/midtermProject/groupProject/CSCI441MP/MPEngine.cpp" "CMakeFiles/MP.dir/MPEngine.cpp.o" "gcc" "CMakeFiles/MP.dir/MPEngine.cpp.o.d"
+  "/home/charlesmowbray/Documents/CSCI441/midtermProject/groupProject/CSCI441MP/Peanut.cpp" "CMakeFiles/MP.dir/Peanut.cpp.o" "gcc" "CMakeFiles/MP.dir/Peanut.cpp.o.d"
+  "/home/charlesmowbray/Documents/CSCI441/midtermProject/groupProject/CSCI441MP/main.cpp" "CMakeFiles/MP.dir/main.cpp.o" "gcc" "CMakeFiles/MP.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
